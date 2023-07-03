4c3a341622040b0f751cb4a4f95f9aa03396f480
ihor <ihor@poczta.onet.pl>
2023-07-03 06:30:51 +0200

    próby na frontendzie

--- src/components/frontend.jsx
+++ src/components/frontend.jsx
@@ -1,51 +1,126 @@
 import React from "react";
 import styles from "./../styles/frontend.module.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Frontend = () => {
   return (
     <div>
-      {/* Row 2*/}
-
-      <div className="col-lg-12">
-        <div className="row">
-          <div className={`col-lg-3 ${styles.innerRow}`}></div>
-          <div className={`col-lg-6 ${styles.innerRow}`}></div>
-          <div className={`col-lg-3 ${styles.innerRow}`}></div>
+      <div class="container"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 bg-dark text-light">
+            <h2>1</h2>
+          </div>
+          <div class="col-md-6 ">
+            <h2>2</h2>
+          </div>
+          <div class="col-md-3 bg-dark text-light">
+            <h2>3</h2>
+          </div>
         </div>
-      </div>
-      {/* Row 3*/}
-
-      <div className="col-lg-12">
-        <div className="row">
-          <div className={"col-lg-9"}>
-            <div className="row ">
-              <div className={`col ${styles.innerRow}`}></div>
-              <div className={`col ${styles.innerRow}`}></div>
+        <div class="row">
+          <div class="col bg-dark  text-light ">
+            <h2>4</h2>
+          </div>
+          <div className="custom-col" style={{ width: "37.5%" }}>
+            <h2>5</h2>
+          </div>
+          <div class="col-md-3 bg-dark  text-light">
+            <h2>6</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div
+            class="col  bg-dark g-0  text-light"
+            style={{ aspectRatio: "1/1" }}
+          >
+            <div className="row g-0" style={{ height: "50%", display: "flex" }}>
+              <div
+                className="col bg-light g-0 text-dark"
+                style={{
+                  width: "calc(33.33%)",
+                  height: 0,
+                  paddingBottom: "calc(33.33%)",
+                }}
+              >
+                {" "}
+                <h2>1</h2>
+              </div>
+              <div
+                className="col bg-white g-0 text-dark"
+                style={{
+                  width: "calc(33.33%)",
+                  height: 0,
+                  paddingBottom: "calc(33.33%)",
+                }}
+              >
+                <h2>2</h2>{" "}
+              </div>
+              <div
+                className="col bg-light g-0 text-dark"
+                style={{
+                  width: "calc(33.33%)",
+                  height: 0,
+                  paddingBottom: "calc(33.33%)",
+                }}
+              >
+                <h2>3</h2>{" "}
+              </div>
+            </div>
+            <div class="row g-0" style={{ height: "50%" }}>
+              <div
+                class=" bg-light g-0  text-light"
+                style={{
+                  width: "calc(50%)",
+                  height: 0,
+                  paddingBottom: "calc(50%)",
+                }}
+              ></div>
             </div>
           </div>
-          <div className={`col-lg-3 ${styles.innerRow}`}></div>
-        </div>
-      </div>
-      {/* Row 4*/}
-
-      <div className="col-lg-12">
-        <div className="row">
-          <div className={`col-lg-6 ${styles.square}`}>
-            <div className={`g-0 row ${styles.squareDevideCol}`}></div>
-            <div className={`g-0 row ${styles.squareDevideCol}`}></div>
-            {/* <div className={`row  ${styles.squareDevideCol}`}>
-              <div className={`col ${styles.littleSquare}`}></div>
-              <div className={`col ${styles.littleSquare}`}></div>
-              <div className={`col ${styles.littleSquare}`}></div>
-            </div> */}
-            {/* <div className={styles.squareDevideCol}>
-              <div className={`col-lg-6 ${styles.littleSquare}`}></div>
+          <div class="col-md-6 ">
+            <div
+              className="bg-dark row g-0"
+              style={{ height: "50%", display: "flex" }}
+            >
+              <div class="row bg-light  g-0" style={{ height: "50%" }}></div>
             </div>
-            <div className={`col-lg-6 ${styles.square}`}></div> */}
+            <div
+              className="bg-dark row g-0"
+              style={{ height: "50%", display: "flex" }}
+            >
+              <div class="row bg-light  g-0" style={{ height: "50%" }}></div>
+            </div>
           </div>
-          <div className={`col-lg-6 ${styles.square}`}></div>
         </div>
       </div>
+      <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      <SwiperSlide><div className="bg-dark" style={{ height: "200px", width:'200px' }}></div></SwiperSlide>
+      
+    </Swiper>
     </div>
   );
 };
